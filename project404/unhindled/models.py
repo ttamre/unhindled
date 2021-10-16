1f5dd93e49bf8b23800229f07dd5fcec86475802
ismaeelBM <ismaeel@ualberta.ca>
2021-10-16 02:53:46 -0600

    Added support for markdown and images

--- project404/unhindled/models.py
+++ project404/unhindled/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.urls import reverse
 from datetime import datetime, date
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -24,23 +25,26 @@ class Author(models.Model):
 class Post(models.Model):
 	CONTENT_TYPES = (
 		("md", "text/markdown"),
-		("txt","text'plain"),
-		("png","image/png;base64"),
-		("jpeg","image/jpeg;base64"),
+		("txt","text/plain"),
 	)
 	ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	author = models.ForeignKey(Author, on_delete=models.CASCADE)
-	contentType = models.CharField(max_length=4, choices=CONTENT_TYPES)
+	contentType = models.CharField(max_length=4, choices=CONTENT_TYPES, default=CONTENT_TYPES[1],null=False)
 	title = models.CharField(max_length=200)
 	description = models.CharField(max_length=500)
 	created_on = models.DateTimeField(auto_now_add=True)
 	#will need to change
-	content = models.TextField()
+	content = models.TextField(blank=True)
+	images = models.ImageField(null=True,blank=True, upload_to='images/')
 	#class Meta:
 		#abstract = True
 
 	def get_absolute_url(self):
 		return reverse('viewPost', args=(str(self.author), self.pk))
+
+	def clean(self):
+		if not (self.images or self.content):
+			raise ValidationError("You must specify either email or telephone")
 
 #maybe use depending on implementation		
 #class PublicPost(Post):
